336dcaea1a038d2d17e75453003e9da59a2d977b
Addison Hill <adddison.hill@gmail.com>
2019-10-29 01:27:08 -0500

    styling done

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Todo = props => {
   return (
     <div
-      className={`item${props.item.completed ? 'completed' : ''}`}
+      className={props.item.completed ? 'item completed' : 'item'}
       onClick={() => props.toggleCompleted(props.item.id)} >
       <p>{props.item.task}</p>
     </div>
